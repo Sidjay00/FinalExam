ce4ea3ecc0d33a043dba8d356e64806f28a91a06
K.Sidorov <2887458@gmail.com>
2023-04-09 15:57:45 +0300

    added arrays

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] string1 = {"hello", "2", "world", ":-)"};
+string[] string2 = {"1234", "1567", "-2", "computer science"};
+string[] string3 = {"Russia", "Denmark", "Kazan"};
+
+Console.WriteLine($"{string1[0]} {string3[2]}");
