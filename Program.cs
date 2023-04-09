cfbba022f60a61b04fcfed50640b541bb5f50389
K.Sidorov <2887458@gmail.com>
2023-04-09 23:23:53 +0300

    added a solution for the final exam task

--- Program.cs
+++ Program.cs
@@ -1,5 +1,28 @@
-﻿string[] string1 = {"hello", "2", "world", ":-)"};
-string[] string2 = {"1234", "1567", "-2", "computer science"};
-string[] string3 = {"Russia", "Denmark", "Kazan"};
+﻿string[] string1 = { "hello", "2", "world", ":-)" };
+string[] string2 = { "1234", "1567", "-2", "computer science" };
+string[] string3 = { "Russia", "Denmark", "Kazan" };
 
-Console.WriteLine($"{string1[0]} {string3[2]}");
+Console.WriteLine($"[{String.Join(", ", string1)}] -> [{String.Join(", ", ShortStringsArr(string1))}]");
+Console.WriteLine($"[{String.Join(", ", string2)}] -> [{String.Join(", ", ShortStringsArr(string2))}]");
+Console.WriteLine($"[{String.Join(", ", string3)}] -> [{String.Join(", ", ShortStringsArr(string3))}]");
+
+string[] ShortStringsArr(string[] str)
+{
+    string[] string1 = new string[str.GetLength(0)];
+    int count = 0;
+    for (int i = 0; i < str.GetLength(0); i++)
+    {
+        if (str[i].Length <= 3)
+        {
+            string1[count] = str[i];
+            count++;
+        }
+    }
+
+    string[] string2 = new string[count];
+    for (int i = 0; i < count; i++)
+    {
+        string2[i] = string1[i];
+    }
+    return string2;
+}
